c32e41aab717fb5a6db4c3cf6dc2b57d1e6a5c01
Suryansh Chauhan <suryanshchauhan2005@gmail.com>
2024-04-07 08:41:00 +0530

    fix: next build issues

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,15 +1,14 @@
 import { wallets } from "@cosmos-kit/keplr-extension";
 import { ChainProvider } from "@cosmos-kit/react";
-import { assets, chains } from "chain-registry";
-import type { AppProps } from "next/app";
-import { mantra } from "../lib/chainConfig";
-import "../styles/globals.css";
-
 import { ThemeProvider } from "@interchain-ui/react";
 import "@interchain-ui/react/styles";
+import { assets, chains } from "chain-registry";
+import type { AppProps } from "next/app";
 import Head from "next/head";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import { mantra } from "../lib/chainConfig";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
